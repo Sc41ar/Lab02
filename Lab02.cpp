da9e20ec6d8994e0ccb64243e06747d718269094
Sc41ar <mrniki2396@gmail.com>
2022-10-01 22:10:19 +0700

    Добавьте файлы проекта.

---
+++ Lab02.cpp
@@ -0,0 +1,17 @@
+﻿// Lab02.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+}
+
+struct Genre
+{
+	private enum Genre
+};
+
